bee932b570ee8cfeec7a4a700f1bea13bb45529e
Thomas Csn <thomas.cosson@etu.univ-st-etienne.fr>
2024-11-28 22:41:49 +0100

    change style login page

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,9 +1,5 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <v-toolbar-title><h1>WE ART</h1></v-toolbar-title>
-    </v-app-bar>
-
     <v-main>
       <v-container>
         <v-row justify="center">
@@ -16,7 +12,10 @@
                     alt="Map logo"
                   ></v-img>
                 </div>
-                <v-form @submit.prevent="submitLogin">
+
+                <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
+
+                <v-form class="formulaire" @submit.prevent="submitLogin">
                   <v-text-field
                     v-model="email"
                     label="Adresse email"
@@ -26,13 +25,18 @@
                   <v-text-field
                     v-model="password"
                     label="Mot de passe"
-                    type="password"
+                    :type="showPassword ? 'text' : 'password'" 
                     required
+                    :append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+                    @click:append="togglePasswordVisibility"
                   ></v-text-field>
                   <div class="pa-5" outlined>
-                    <v-btn type="submit" color="primary" >Se connecter</v-btn>
+                    <v-btn type="submit" color="primary" >
+                      Me connecter
+                      <v-icon right>mdi-arrow-right</v-icon>
+                    </v-btn>
                   </div>
-                  <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
+                  <p>Nouveau sur We Art? <router-link to="/signup">Je m'inscris</router-link></p>
                 </v-form>
               </v-card-text>
             </div>
@@ -55,7 +59,8 @@ export default {
       email: '',
       password: '',
       Logo,
-      errorMessage: ''
+      errorMessage: '',
+      showPassword: false,
     }
   },
   computed: {
@@ -66,7 +71,10 @@ export default {
 
   methods: {
     ...mapActions(['login']), // Importer l'action login de Vuex
-   
+    togglePasswordVisibility() {
+      this.showPassword = !this.showPassword;
+    },
+
     async submitLogin() {
       try {
         //const response = await axios.post('http://localhost:3000/api/users/login', {
@@ -123,10 +131,20 @@ height: auto;    /* Maintain aspect ratio */
 width: 300px;    /* Set a fixed width */
 display: block;  /* Ensure it behaves like a block element */
 margin: 0 auto;  /* Center horizontally */
+margin-top: 30%;
 }
 
 .v-btn {
-color: white;
-border-radius: 20px !important;;
+color: red;
+border-radius: 20px !important;
+padding-left: 10% !important;
+padding-right: 10% !important;
+padding-top: 7% !important;
+padding-bottom: 6% !important;
+
+}
+
+.formulaire {
+  margin-top: 40% !important;
 }
 </style>
